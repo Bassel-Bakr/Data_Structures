d9c57758682f59956422f0da173b9e435e53ed6a
Bassel Bakr <bassel.bakr@gmail.com>
2017-09-24 12:11:19 +0200

    Update string_hash.cpp

--- CPP/string_hash.cpp
+++ CPP/string_hash.cpp
@@ -1,39 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<int p=31, int mod=1000*1000*1000+7>
-struct string_hash {
-  using ll = long long;
-
+template<int p, int mod>
+class string_hash {
+public:
   int n;
   vector<int> f;
   vector<int> h;
+
+  constexpr static bool is_mersenne(ll x) {
+    return not (x&(x+1));
+  }
+
+  constexpr static bool is_power_of_two(ll x) {
+    return not (x&(x-1));
+  }
+
+  template<int z = mod, int o = __builtin_popcount(z)>
+  inline static enable_if_t<is_mersenne(z), ll> fix(ll x) {
+    x = (x >> o) + (x & mod);
+    x = (x >> o) + (x & mod);
+    return x;
+  }
+
+  template<int z = mod> 
+  inline static enable_if_t<is_power_of_two(z), ll> fix(ll x) {
+    return x & (mod-1);
+  }
+ 
+  template<int z = mod> 
+  inline static enable_if_t<not (is_power_of_two(z) || is_mersenne(z)), ll> fix(ll x) {
+    return x%mod;
+  }
 
   template<class iter> 
   static int hash_of(iter l, iter r) {
     ll x = 0;
     for(iter it = l; it != r; ++it)
-      x = (x*p + *it - 'a' + 1) % mod;
+      x = fix(x*p + *it - 'a' + 1);
     return x;
   }
 
   template<class iter>
   string_hash(iter l, iter r) {
     n = distance(l, r);
+    assert(n >= 0);
     h.assign(n+1, 0);
     f.assign(n+1, 1);
 
     ll x = 1, y = 0;
     iter it = l;
-    for(int i = 0; i < n; ++i) {
-      f[i+1] = x = x * p % mod;
-      h[i+1] = y = (y*p + *it++ - 'a' + 1) % mod;
+    for(int i = 0; i < n; ++i, ++it) {
+      f[i+1] = x = fix(x * p);
+      h[i+1] = y = fix(y*p + (*it) - 'a' + 1);
     }
   }
 
   int query(int l, int r) {
-    ll x = h[r+1] - 1ll * h[l] * f[r-l+1] % mod;
-    return (x + mod) % mod;
+    ll x = fix(h[r+1] - fix(1ll * h[l] * f[r-l+1]));
+    return fix(x+mod);
   }
 };
 
