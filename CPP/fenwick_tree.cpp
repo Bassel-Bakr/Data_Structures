3bf4cee63f20bbaa93ce3f183b505741162b0a6f
Bassel Bakr <bassel.bakr@gmail.com>
2017-10-09 06:54:18 +0200

    Update fenwick_tree.cpp

--- CPP/fenwick_tree.cpp
+++ CPP/fenwick_tree.cpp
@@ -1,6 +1,7 @@
+template<class T>
 struct fenwick {
   int n;
-  vector<ll> a, b;
+  vector<T> a, b;
 
   fenwick(int s) {
     n = s;
@@ -8,30 +9,30 @@ struct fenwick {
     b.assign(n+1, 0);
   }
 
-  void update(vector<ll>& bit, int idx, ll v) {
+  void update(vector<T>& bit, int idx, T v) {
     for(int i = idx; i <= n; i += (i&-i))
       bit[i] += v;
   }
 
-  void update(int l, int r, ll v) {
+  void update(int l, int r, T v) {
     update(a, l, v);
     update(a, r+1, -v);
     update(b, l, -v*(l-1));
     update(b, r+1, v*r);
   }
 
-  ll query(vector<ll> const& bit, ll idx) {
-    ll sum = 0;
+  T query(vector<T> const& bit, T idx) {
+    T sum = 0;
     for(int i = idx; i > 0; i -= (i&-i))
       sum += bit[i];
     return sum;
   }
 
-  ll query(int idx) {
+  T query(int idx) {
     return query(a, idx) * idx + query(b, idx);
   }
 
-  ll query(int l, int r) {
+  T query(int l, int r) {
     return query(r) - query(l-1);
   }
 };
